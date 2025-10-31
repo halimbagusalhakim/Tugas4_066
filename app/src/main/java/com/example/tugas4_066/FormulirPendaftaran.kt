a6b98d43104be2b6a84f12139f6f3966e648aa44
halimbagusalhakim <halsat132@gmail.com>
2025-10-31 23:00:27 +0700

    memperbaiki erro

--- app/src/main/java/com/example/tugas4_066/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/tugas4_066/FormulirPendaftaran.kt
@@ -1,13 +1,29 @@
 package com.example.tugas4_066
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -21,5 +37,37 @@ fun FormulirPendaftaran(modifier: Modifier){
     val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
     val paddingLarge = dimensionResource(id = R.dimen.padding_large)
     val buttonHeight = dimensionResource(id = R.dimen.button_height)
+
+    Surface(
+        color = Color(0xFFF6ECFF),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 53.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .statusBarsPadding()
+                    .background(Color(0xFFB56BFF)) // warna ungu full tepi layar
+                    .padding(vertical = paddingLarge)
+            ) {
+                Text(
+                    text = stringResource(R.string.FormulirPendaftaran),
+                    fontSize = dimensionResource(id = R.dimen.font_title).value.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+
+
+                )
+            }
+        }
+    }
 
 }
