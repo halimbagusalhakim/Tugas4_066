e1f70fcc0e091906440ccf3e19c6c5dd313ab8a9
halimbagusalhakim <halsat132@gmail.com>
2025-10-31 23:02:00 +0700

    membuat card yang diberikan beberapa modifier agar lebih bagus dan rapi

--- app/src/main/java/com/example/tugas4_066/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/tugas4_066/FormulirPendaftaran.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -70,6 +74,18 @@ fun FormulirPendaftaran(modifier: Modifier){
                 )
             }
             Spacer(modifier = Modifier.height(20.dp))
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = paddingMedium)
+                    .wrapContentHeight(),
+                shape = RoundedCornerShape(16.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+            ){
+
+            }
 
         }
     }
