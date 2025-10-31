7e67526192fdd5cecf8e33920c4b5830333d044a
halimbagusalhakim <halsat132@gmail.com>
2025-10-31 22:54:46 +0700

    membuat funsgi FormulirPendaftaran

--- app/src/main/java/com/example/tugas4_066/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/tugas4_066/FormulirPendaftaran.kt
@@ -1,1 +1,10 @@
 package com.example.tugas4_066
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun FormulirPendaftaran(modifier: Modifier){
+
+}
