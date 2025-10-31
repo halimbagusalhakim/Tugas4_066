26ec4702c284a5285a5eb703a8d339c31a8b8e55
halimbagusalhakim <halsat132@gmail.com>
2025-10-31 22:55:37 +0700

    membuat variable nama, alamat, jenisKelamin, dan statusPerkawinan

--- app/src/main/java/com/example/tugas4_066/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/tugas4_066/FormulirPendaftaran.kt
@@ -1,10 +1,19 @@
 package com.example.tugas4_066
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier){
+    var nama by remember { mutableStateOf(TextFieldValue("")) }
+    var alamat by remember { mutableStateOf(TextFieldValue("")) }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var statusPerkawinan by remember { mutableStateOf("") }
 
 }
